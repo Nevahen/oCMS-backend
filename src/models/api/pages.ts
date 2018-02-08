056f1db389dee63067675f41afa26e8d6953caeb
Oskari Nevalainen <oskari.nevalainen@gmail.com>
2018-02-08 13:35:07 +0200

    it works, but ain't pretty - saving and removing tags

--- src/models/api/pages.ts
+++ src/models/api/pages.ts
@@ -1,17 +1,17 @@
 import * as db from '../../db';
 import { DatabaseConnection } from '../../db';
-import {ApiError} from '../../ApiError';
+import { ApiError } from '../../ApiError';
 import { Page } from '../page';
 import { ApiResponse } from '../../ApiResponse';
 import { Utils } from '../../utils/utils';
 import { QueryUtils } from '../../utils/QueryUtils';
 import { resolve } from 'path';
 
-export class Pages{
+export class Pages {
 
- private db;
+    private db;
 
-    constructor(){
+    constructor() {
         this.db = DatabaseConnection.Instance.connection;
     }
 
@@ -19,12 +19,12 @@ export class Pages{
      * Get page by id
      * @param id Page ID
      */
-    getPageByID(id){
+    getPageByID(id) {
 
         let pagedata;
         const sql = 'select * from ocms_pages where page_id = ?';
 
-        return new Promise((resolve,reject)=>{
+        return new Promise((resolve, reject) => {
             /*
              * Flow: Check valid int and that page exists
              * Get Basic Pagedata
@@ -32,47 +32,48 @@ export class Pages{
              * Catch errors and return them if something fails
              */
             Utils.isInt(id)
-            .then(() => { return QueryUtils.PageExists(id)
-            })
-            .then(() => { return QueryUtils.Query(sql,[id])})
-            .then(results =>{
-                pagedata = results;
-                // Init tag array
-                pagedata[0].tags = [];
-                
-            })
-            .then(results =>{
-                return this.getPageTags(id)
-            })
-            .then(tags =>{    
+                .then(() => {
+                    return QueryUtils.PageExists(id)
+                })
+                .then(() => { return QueryUtils.Query(sql, [id]) })
+                .then(results => {
+                    pagedata = results;
+                    // Init tag array
+                    pagedata[0].tags = [];
 
-                for(let tag of tags){
-                    pagedata[0].tags.push(tag.tag_name);   
-                }
+                })
+                .then(results => {
+                    return this.getPageTags(id)
+                })
+                .then(tags => {
 
-               resolve(pagedata);
-            })
-            .catch(err =>{
-                reject(err);
-            })        
+                    for (let tag of tags) {
+                        pagedata[0].tags.push(tag.tag_name);
+                    }
+
+                    resolve(pagedata);
+                })
+                .catch(err => {
+                    reject(err);
+                })
         })
-}
+    }
 
     /**
      * Returns all pages
      */
-    getAllPages(){
+    getAllPages() {
 
-        return new Promise((resolve,reject)=>{
+        return new Promise((resolve, reject) => {
             const sql = "select * from ocms_pages";
             QueryUtils.Query(sql).
-            then((pages)=>{
-                resolve(pages);
-            })
-            .catch((err)=>{
-                reject(err);
-            })
-        }) 
+                then((pages) => {
+                    resolve(pages);
+                })
+                .catch((err) => {
+                    reject(err);
+                })
+        })
     };
 
     /**
@@ -83,44 +84,48 @@ export class Pages{
      * @returns ApiError or ApiResponse
      * @memberof Pages
      */
-    updatePage(data:Page){
-        return new Promise((resolve,reject)=>{
+    updatePage(data: Page) {
+        return new Promise((resolve, reject) => {
 
             let sql = "UPDATE ocms_pages SET content = ?, title = ?, lastedit=now() where page_id = ?";
             QueryUtils.PageExists(data.page_id)
-            .then(()=>{
-            return QueryUtils.Query(sql,
-                [
-                    data.content,
-                    data.title,data.page_id
-                ])
-            })
-            .then(()=>{
-              if(data.tags && data.tags.length > 0){
-                  return this.setPageTags(data.tags)                  
-              }
-              
-              return null;
-            })
-            .then((tag_ids) =>{
-                if(tag_ids){
-                    console.log(tag_ids);
-                }
-                else{
-                    console.log("no tags");
-                }
-            })
-            .then(()=>{
-                resolve(new ApiResponse(200,"All good!"));
-            })
-            .catch((err)=>{
-                reject(new ApiError(500,err));
-            })
+                .then(() => {
+                    return QueryUtils.Query(sql,
+                        [
+                            data.content,
+                            data.title, data.page_id
+                        ])
+                })
+                .then(() => {
+                    if (data.tags && data.tags.length > 0) {
+                        return this.setPageTags(data.tags)
+                    }
+                    return null;
+                })
+                .then((tag_ids) => {
+                    if (tag_ids) {
+                        let array = [];
+                        for (let i = 0; i < tag_ids[1].length; i++) {
+                            array.push(tag_ids[1][i].tag_id);
+                        }
+
+                        return this.setTagRelations(data.page_id, array);
+                    }
+                    else {
+                        return this.removeAllTagRelations(data.page_id);
+                    }
+                })
+                .then(() => {
+                    resolve(new ApiResponse(200, "All good!"));
+                })
+                .catch((err) => {
+                    reject(new ApiError(500, err));
+                })
         });
     }
 
-    createPage(data:Page){
-        return new Promise((resolve,reject)=>{
+    createPage(data: Page) {
+        return new Promise((resolve, reject) => {
 
             let sql = "INSERT into ocms_pages SET content = ?, title = ?";
 
@@ -128,36 +133,36 @@ export class Pages{
                 [
                     data.content,
                     data.title
-                ])                
-            .then(()=>{
-                resolve(new ApiResponse(200,"All good!"));
-            })
-            .catch(()=>{
-                reject(new ApiError(500,"Internal Server Error"));
-            })      
+                ])
+                .then(() => {
+                    resolve(new ApiResponse(200, "All good!"));
+                })
+                .catch(() => {
+                    reject(new ApiError(500, "Internal Server Error"));
+                })
         });
     }
-    
-    deletePage(page_id:number){
-        
+
+    deletePage(page_id: number) {
+
         const sql = "DELETE from ocms_pages WHERE page_id = ?";
 
-        return new Promise((resolve,reject)=>{
+        return new Promise((resolve, reject) => {
 
             QueryUtils.PageExists(page_id)
-            .then(()=>{
-                return QueryUtils.Query(sql,[page_id])
-            })
-            .then(()=>{
-                resolve(new ApiResponse(400, "All good!"));
-            })
-            .catch((err)=>{
-                reject(new ApiError(500,err));
-            })
-        });        
+                .then(() => {
+                    return QueryUtils.Query(sql, [page_id])
+                })
+                .then(() => {
+                    resolve(new ApiResponse(400, "All good!"));
+                })
+                .catch((err) => {
+                    reject(new ApiError(500, err));
+                })
+        });
     }
 
-    getPageTags(page_id:number){
+    getPageTags(page_id: number) {
 
         const sql = `
         select tag_name from ocms_tags
@@ -167,17 +172,39 @@ export class Pages{
         return QueryUtils.Query(sql, [page_id])
     }
 
-    setPageTags(tags:Array<string>){
+    setTagRelations(page_id, tags) {
         // Extract this to function..
         let escaped = "";
-        for(let i in tags){
-        DatabaseConnection.Instance.connection.escape(tags[i]);
-        escaped += "("+DatabaseConnection.Instance.connection.escape(tags[i])+"),";
+        for (let i in tags) {
+            DatabaseConnection.Instance.connection.escape(tags[i]);
+            escaped += `(${page_id}, ` + DatabaseConnection.Instance.connection.escape(tags[i]) + "),";
         }
-        escaped = escaped.slice(0,-1);
+        escaped = escaped.slice(0, -1);
+        
+        const sql = `
+        INSERT IGNORE into ocms_page_tags (page_id, tag_id)
+         values  ${escaped}; DELETE from ocms_page_tags
+          where page_id = ? and tag_id not in (?)`;
+        console.log(sql);
+        return QueryUtils.Query(sql, [page_id, tags]);
+    }
+
+    removeAllTagRelations(page_id) {
+        const sql = "DELETE from ocms_page_tags where page_id = ?";
+        return QueryUtils.Query(sql, [page_id]);
+    }
+
+    setPageTags(tags: Array<string>) {
+        // Extract this to function..
+        let escaped = "";
+        for (let i in tags) {
+            DatabaseConnection.Instance.connection.escape(tags[i]);
+            escaped += "(" + DatabaseConnection.Instance.connection.escape(tags[i]) + "),";
+        }
+        escaped = escaped.slice(0, -1);
         //
 
-        const sql = "INSERT IGNORE into ocms_tags (tag_name) values " + escaped+"; select tag_id from ocms_tags where tag_name in (?);";
+        const sql = "INSERT IGNORE into ocms_tags (tag_name) values " + escaped + "; select tag_id from ocms_tags where tag_name in (?);";
 
         return QueryUtils.Query(sql, [tags]);
     }
--- src/utils/QueryUtils.ts
+++ src/utils/QueryUtils.ts
@@ -1,42 +1,40 @@
 import { DatabaseConnection } from '../db';
 import { ApiError } from '../ApiError';
 
-export class QueryUtils{
+export class QueryUtils {
 
-    public static async PageExists(id:number){
+    public static async PageExists(id: number) {
 
-        return new Promise((resolve,reject)=>{
+        return new Promise((resolve, reject) => {
 
             const sql = "SELECT page_id from ocms_pages where page_id = ?";
-            DatabaseConnection.Instance.connection.query(sql,[id], (err,results) => {
-                if(err){
+            DatabaseConnection.Instance.connection.query(sql, [id], (err, results) => {
+                if (err) {
                     reject(err.code)
                 }
-                if(results.length == 1){
+                if (results.length == 1) {
                     resolve(true);
                 }
-                else{
+                else {
                     reject(new ApiError(400, "Element doesn't exist"));
                 }
             });
         })
     }
 
-    public static Query(sql:string, values?:Array<any>):Promise<any>{
+    public static Query(sql: string, values?: Array<any>): Promise<any> {
 
 
-        return new Promise((resolve,reject)=>{
-            DatabaseConnection.Instance.connection.query(sql,values,
-                (err,results)=>{
-                    if(err){
-                        console.log(err);
-                        console.log(err.sql)
+        return new Promise((resolve, reject) => {
+            DatabaseConnection.Instance.connection.query(sql, values,
+                (err, results) => {
+                    if (err) {
                         reject(err);
                     }
-                    else{
+                    else {
                         resolve(results);
                     }
-            })
-       })
+                })
+        })
     }
 }
